fd2becb397ce98910009d825c3ffc756d4fbc14e
Mateus Garcia <mateus.gigainfo@gmail.com>
2019-05-16 17:55:10 -0300

    ci(gitlab): ci configurado

--- src/common/redis.config.ts
+++ src/common/redis.config.ts
@@ -1,5 +1,2 @@
 export const redisHost: string = process.env.TRAJETOS_REDIS_HOST;
 export const redisPort: number = Number( process.env.TRAJETOS_REDIS_PORT );
-export const redisDicionario: string = process.env.TRAJETOS_REDIS_DICIONARIO;
-export const redisRaio: number = Number( process.env.TRAJETOS_REDIS_RAIO );
-export const redisCacheTime: number = Number( process.env.TRAJETOS_REDIS_CACHE_TIME ) || 600; // 10 minutos default

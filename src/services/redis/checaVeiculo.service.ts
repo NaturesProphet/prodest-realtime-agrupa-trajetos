c1bf8532876c4cc07b8d7f871abe47d01220141d
Mateus Garcia <mateus.gigainfo@gmail.com>
2019-05-17 20:32:34 -0300

    chore(ajsutes): ajustes

--- src/services/redis/checaVeiculo.service.ts
+++ src/services/redis/checaVeiculo.service.ts
@@ -11,7 +11,7 @@ bluebird.promisifyAll( redis.Multi.prototype );
 
 export async function checaVeiculo ( rotulo: string, redisConnection ): Promise<string> {
   try {
-    let itinerario = await redisConnection.getAsync( rotulo );
+    let itinerario = await redisConnection.getAsync( `carro:${rotulo}` );
     return itinerario;
   } catch ( err ) {
     console.log( `[ checaVeiculo ] Erro ao tentar buscar rotulo no redis. ${err.message}` );
--- src/services/redis/salvaVeiculo.service.ts
+++ src/services/redis/salvaVeiculo.service.ts
@@ -11,7 +11,13 @@ bluebird.promisifyAll( redis.Multi.prototype );
 
 export async function salvaVeiculo ( veiculo: Veiculo, redisConnection ) {
   try {
-    await redisConnection.setAsync( veiculo.ROTULO, veiculo.ITINERARIO );
+    if ( veiculo.ROTULO != undefined ) {
+      await redisConnection.setAsync( `carro:${veiculo.ROTULO}`, veiculo.ITINERARIO );
+    }
+    else {
+      console.log( `Rotulo invalido: ${JSON.stringify( veiculo )}` );
+    }
+
   } catch ( err ) {
     console.log( `[ salvaVeiculo ]: Erro ao enviar um veiculo ao Redis. ${err.message}` );
   }
